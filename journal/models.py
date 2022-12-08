b69f3419c5fac7f4769c0773f155f9c62dfea8ac
Evan2022 <evan201198@hotmail.com>
2022-12-08 17:04:51 +0000

    create journal and basic trade model

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+
+class Journal(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    starting_balance = models.DecimalField(max_digits=15, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Trade(models.Model):
+    profit_loss = models.DecimalField(max_digits=15, decimal_places=2)
+    time = models.TimeField()
+    date = models.DateField()
+    journal = models.ForeignKey(Journal, on_delete=models.CASCADE)
+
