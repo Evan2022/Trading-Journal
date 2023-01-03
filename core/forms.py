986863e461df6f2f9ea9a16009f4b41ca79258af
Evan2022 <evan201198@hotmail.com>
2023-01-03 17:08:58 +0000

    create custom signupform and add basic style

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,4 @@
-from allauth.account.forms import LoginForm
+from allauth.account.forms import LoginForm, SignupForm
 from django import forms
 
 
@@ -9,3 +9,20 @@ class CustomLoginForm(LoginForm):
         self.fields['password'].widget = forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Password'})
         self.fields['login'].label = ''
         self.fields['password'].label = ''
+
+
+class CustomSignupForm(SignupForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].widget = forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'First Name'})
+        self.fields['last_name'].widget = forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Last Name'})
+        self.fields['email'].widget = forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'Email'})
+        self.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'})
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Password'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Confirm Password'})
+        self.fields['first_name'].label = ''
+        self.fields['last_name'].label = ''
+        self.fields['email'].label = ''
+        self.fields['username'].label = ''
+        self.fields['password1'].label = ''
+        self.fields['password2'].label = ''
--- core/views.py
+++ core/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from .forms import CustomLoginForm
+from .forms import CustomLoginForm, CustomSignupForm
 
 
 def login_view(request):
     form = CustomLoginForm()
     context = {'form': form}
     return render(request, 'login.html', context)
+
+
+def signup_view(request):
+    form = CustomSignupForm()
+    context = {'form': form}
+    return render(request, 'signup.html', context)
