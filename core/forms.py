1386a72d1185c2be044991a21c7c92d7a82a5e34
Evan2022 <evan201198@hotmail.com>
2023-01-04 17:26:07 +0000

    removed first name and last name from sign up form

--- core/forms.py
+++ core/forms.py
@@ -14,14 +14,10 @@ class CustomLoginForm(LoginForm):
 class CustomSignupForm(SignupForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['first_name'].widget = forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'First Name'})
-        self.fields['last_name'].widget = forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Last Name'})
         self.fields['email'].widget = forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'Email'})
         self.fields['username'].widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'})
         self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Password'})
         self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Confirm Password'})
-        self.fields['first_name'].label = ''
-        self.fields['last_name'].label = ''
         self.fields['email'].label = ''
         self.fields['username'].label = ''
         self.fields['password1'].label = ''
