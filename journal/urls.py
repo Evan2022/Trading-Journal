8c36203126aaaa6baeea8fbad2ed91bf7bd0a217
Evan2022 <evan201198@hotmail.com>
2023-01-09 20:53:50 +0000

    set up basic urls, view for journals.html

--- journal/urls.py
+++ journal/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from allauth.account.views import LoginView
+from .views import journals, trades
 
 
 urlpatterns = [
     path('', LoginView.as_view(), name='home'),
     path('accounts/', include('allauth.urls')),
+    path('journals/', journals, name='journals'),
 ]
--- journal/views.py
+++ journal/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.forms import modelformset_factory
-from .forms import CustomLoginForm, CustomSignupForm
+from core.forms import CustomLoginForm, CustomSignupForm
 from journal.models import Journal, Trade
 
 
@@ -14,3 +13,11 @@ def signup_view(request):
     form = CustomSignupForm()
     context = {'form': form}
     return render(request, 'signup.html', context)
+
+
+def journals(request):
+    return render(request, 'journals.html',)
+
+
+def trades(request):
+    return render(request, 'trades.html',)
