09679f373993a1ef731e883b0c0d2c1d84a5daa2
Evan2022 <evan201198@hotmail.com>
2023-01-12 05:13:14 +0000

    add currency options to Journal model, display them in journal and trade templates

--- journal/models.py
+++ journal/models.py
@@ -4,14 +4,24 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
-
 class Journal(models.Model):
+    SYMBOLS = {'USD': '$', 'EUR': '€', 'JPY': '¥', 'GBP': '£'}
+    CURRENCY_CHOICE = (
+        ('USD', SYMBOLS['USD'] + ' USD'),
+        ('EUR', SYMBOLS['EUR'] + ' EUR'),
+        ('JPY', SYMBOLS['JPY'] + ' JPY'),
+        ('GBP', SYMBOLS['GBP'] + ' GBP'),
+    )
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICE)
     name = models.CharField(max_length=200, unique=True)
     starting_balance = models.DecimalField(max_digits=15, decimal_places=2)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+    def symbol(self):
+        return self.SYMBOLS.get(self.currency)
 
 
 class Trade(models.Model):
